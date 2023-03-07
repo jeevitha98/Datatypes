94e0fe2fd7d0754118a196fe1624f1d9673f1c03
jeevitha98 <jeevithahj3@gmail.com>
2023-03-08 02:34:32 +0530

    datatypes added- int, float, string, boolean

--- numerictypes.py
+++ numerictypes.py
@@ -8,7 +8,37 @@ b=20.7
 print(b)
 print(type(b))
 
+#string
+g="good day"
+print(g)
+print(type(g))
+
 #complex
 c=6j+7
 print(c)
 print(type(c))
+
+#binary
+d=0B1011
+print(d)
+print(type(d))
+
+#boolean
+print("2>3")
+print(2>3)
+print(type(2>3))
+
+#type conversion->string to float
+e=(float("20.5"))
+print(e)
+
+#type conversion->binary, hexadecimal, octagonal
+f=bin(23)
+print(f)
+print(type(f))
+
+h=hex(34)
+print(h)
+
+i=oct(33)
+print(i)
