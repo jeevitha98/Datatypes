03f0975f67cd6059a13ab0cf5aa28135cd3fed6f
jeevitha98 <jeevithahj3@gmail.com>
2023-03-09 01:21:30 +0530

    string - indexing,length,slicing

--- stringtype.py
+++ stringtype.py
@@ -9,3 +9,11 @@ print(s*3)
 
 #length of string
 print(len(s))
+
+#slicing
+print(s[8:12]) #from index 8 to 12
+print(s[:8]) #from start till 8th char
+print(s[3:-1]) #from 8th char till end of str
+
+print(s[3::-1]) #3rd arg for reversing string
+print(s[::-1]) #complete string is reversed
