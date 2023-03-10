a983095153484e07a4b97c81d61839f12385aa00
jeevitha98 <jeevithahj3@gmail.com>
2023-03-11 04:13:10 +0530

    tuple

---
+++ tupletype.py
@@ -0,0 +1,14 @@
+tpl=(100,30,56,"abc")
+print(tpl)
+print(tpl[3])
+print(tpl*2)
+print(tpl.count(40))
+print(tpl.index("abx"))
+
+lst=[65,22,"xyz"]
+print(type(lst))
+tpl1=tuple(lst)
+print(type(tpl1))
+
+
+
