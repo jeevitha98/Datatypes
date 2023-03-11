b8e2174a6710c4c6b1a80bbcef3a9bd6d5eff07a
jeevitha98 <jeevithahj3@gmail.com>
2023-03-12 01:20:57 +0530

    tuple

--- tupletype.py
+++ tupletype.py
@@ -1,14 +1,15 @@
-tpl=(100,30,56,"abc")
+tpl=(100,30,56,"gfh")
 print(tpl)
 print(tpl[3])
 print(tpl*2)
 print(tpl.count(40))
-print(tpl.index("abx"))
+print(tpl.index("gfh"))
 
 lst=[65,22,"xyz"]
 print(type(lst))
 tpl1=tuple(lst)
 print(type(tpl1))
+print(tpl1)
 
 
 
